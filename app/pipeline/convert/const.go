732158ba846643a117cc3aafcdefc4fb296e179d
Karthik Natarajan <karthik.n1@mrcooper.com>
2025-12-28 14:59:53 +0530

    Fix pipeline

--- app/pipeline/convert/const.go
+++ app/pipeline/convert/const.go
@@ -1,11 +1,14 @@
 package convert
 
+import (
+	"fmt"
+
+	"github.com/cloudness-io/cloudness/version"
+)
+
 const (
 	// images
 	busyBoxImage = "busybox:1.37.0"
-
-	//build
-	baseImage = "cloudnessio/builder:sha-35ec1d1"
 
 	// workspace volume
 	wsBuildVolumePath = "/cloudness/workspace/build"
@@ -13,3 +16,16 @@ const (
 	//deployment workspace volume
 	wsDeployVolumePath = "/cloudness/workspace/deploy"
 )
+
+var (
+	//build
+	baseImage = getBuilderImage()
+)
+
+func getBuilderImage() string {
+	ver := version.Version
+	if ver.Major == 0 && ver.Minor == 0 && ver.Patch == 0 {
+		return "cloudnessio/builder:latest"
+	}
+	return fmt.Sprintf("cloudnessio/builder:v%d.%d.%d", ver.Major, ver.Minor, ver.Patch)
+}
