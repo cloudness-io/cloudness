3c9c42b912ee20e59748660b216c77f648b0e11e
KArtHiK <karthiknatarajan04@gmail.com>
2026-01-22 17:29:25 +0000

    Support updating application name from header (#39)

--- app/router/web.go
+++ app/router/web.go
@@ -361,6 +361,7 @@ func setupApplication(r chi.Router, appCtx context.Context, envCtrl *environment
 			//Inject application here
 			r.Use(middlewareinject.InjectApplication(appCtrl))
 			r.Get("/", handlerapplication.HandleListDeployments(appCtrl, deploymentCtrl))
+			r.Patch("/name", handlerapplication.HandleUpdateName(appCtrl))
 			r.Patch("/icon", handlerapplication.HandleUpdateIcon(appCtrl))
 			r.Get("/deployments", handlerapplication.HandleListDeployments(appCtrl, deploymentCtrl))
 			r.Get("/settings", handlerapplication.HandleGetSettings(appCtrl, ghAppCtrl))
