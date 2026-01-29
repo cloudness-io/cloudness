c2faafca00258039fd654c546040c5a57401fa25
KArtHiK <karthiknatarajan04@gmail.com>
2026-01-29 10:30:32 +0000

    Remove timescaledb properitary components (#52)

--- app/store/database/migrate/postgres/0002_create_table_a_app_metrics.up.sql
+++ app/store/database/migrate/postgres/0002_create_table_a_app_metrics.up.sql
@@ -1,6 +1,6 @@
- CREATE EXTENSION IF NOT EXISTS timescaledb;
+CREATE EXTENSION IF NOT EXISTS timescaledb;
 
- CREATE TABLE app_metrics (
+CREATE TABLE app_metrics (
   metric_timestamp        TIMESTAMPTZ       NOT NULL,
   metric_application_uid  BIGINT            NOT NULL,
   metric_instance_name    TEXT              NOT NULL,
@@ -15,23 +15,4 @@ SELECT create_hypertable(
   'metric_timestamp',
   chunk_time_interval => INTERVAL '1 day',
   if_not_exists => true
-);
-
-CREATE INDEX app_metrics_app_instance_time
-ON app_metrics (metric_application_uid, metric_instance_name, metric_timestamp DESC);
-
-ALTER TABLE app_metrics
-SET (
-  timescaledb.compress,
-  timescaledb.compress_segmentby = 'metric_application_uid, metric_instance_name'
-);
-
-SELECT add_compression_policy(
-  'app_metrics',
-  INTERVAL '7 days'
-);
-
-SELECT add_retention_policy(
-  'app_metrics',
-  INTERVAL '30 days'
 );
